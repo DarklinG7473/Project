575ee1ecd913a8dde57dec406e39e483ebd71055
AssylzhanK <karibaev.a7473@gmail.com>
2022-12-15 10:13:25 +0500

    Game loop

--- PyGameProject.py
+++ PyGameProject.py
@@ -11,3 +11,29 @@ from win32api import GetSystemMetrics
 # pygame initializing
 pygame.init()
 mixer.init()
+
+# main screen
+screen = pygame.display.set_mode((1500, 826))
+background = pygame.image.load('hockey.jpg')
+pygame.display.set_caption('Air Hockey')
+
+# icon of the main screen
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
+
+
+
+
+bool = True
+while bool:
+
+    screen.blit(background, (0, 0))
+    ball()
+    
+    
+    
+    player1()
+    player2()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            bool = False
