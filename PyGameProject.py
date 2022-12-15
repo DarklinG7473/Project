17d9baa9e3ced5417121aac33ac2886a2dfa181e
AssylzhanK <karibaev.a7473@gmail.com>
2022-12-15 10:09:59 +0500

    Importing libraries & initializing pygame

---
+++ PyGameProject.py
@@ -0,0 +1,13 @@
+import pygame
+import random
+import sys
+import pygame as pg
+from PIL import Image, ImageDraw
+from pygame import mixer
+from pygame.locals import *
+from win32api import GetSystemMetrics
+
+
+# pygame initializing
+pygame.init()
+mixer.init()
