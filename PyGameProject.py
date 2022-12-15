7cb77aa53ad4797dd226ed3d8bba130fee896df2
AssylzhanK <karibaev.a7473@gmail.com>
2022-12-15 10:14:56 +0500

    Adding Objects

--- PyGameProject.py
+++ PyGameProject.py
@@ -21,6 +21,35 @@ pygame.display.set_caption('Air Hockey')
 icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
+# moving the ball
+def ball():
+    global ballY, ballX, ballspeedY, ballspeedX, ball_rect, text1, text2, screen, text1rect, text2rect, log, score1, score2, screen
+    if log == 0:
+        score1 = score1 - 1
+        score2 = score2 - 1
+        log = log + 1
+
+    screen.blit(ball_icon, (ballX, ballY))
+
+# blue player's characteristics 
+player1_icon = pygame.image.load('playerleft.png')
+player1_icon = pg.transform.scale(player1_icon, (100, 100))
+player1X = 300
+player1Y = 350
+player1speedx = 0
+player1speedy = 0
+def player1():
+    screen.blit(player1_icon, (player1X, player1Y))
+
+# red player's characteristics
+player2_icon = pygame.image.load('playerright.png')
+player2_icon = pg.transform.scale(player2_icon, (100, 100))
+player2X = 1100
+player2Y = 350
+player2speedx = 0
+player2speedy = 0
+def player2():
+    screen.blit(player2_icon, (player2X, player2Y))
 
 
 
